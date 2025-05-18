21db3333f884868bfb5876f32b928c0927b83192
SimarpreetSidhu <simarpreetsidhu18@gmail.com>
2025-05-18 13:24:50 -0400

    complete word search pair programming assignment

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,12 +15,12 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,8 +31,36 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'QFQEYNAKQ');
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the letters array is completely empty", function() {
+    const result = wordSearch([], 'FRANK');
+
+    assert.isFalse(result);
+  });
+  it("should return false if the letters array contains only empty rows", function() {
+    const result = wordSearch([[],[]], 'FRANK');
+
+    assert.isFalse(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const transposedMatrix = [];
 
-module.exports = wordSearch
+  if (letters.length === 0 || letters[0].length === 0) {
+    return false;
+  }
+  const rows = letters.length;
+  const columns = letters[0].length;
+ 
+  for (let col = 0; col < columns; col++) {
+    const newRow = [];
+    for (let row = 0; row < rows; row++) {
+      newRow.push(letters[row][col]);
+                  
+    }
+    transposedMatrix.push(newRow);
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  const verticalJoin = transposedMatrix.map(ls => ls.join(''));
+   
+  for (const line of horizontalJoin) {
+    if (line.includes(word)) {
+      return true;
+    }
+  }
+  for (const line of verticalJoin) {
+    if (line.includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+
+};
+
+module.exports = wordSearch;
